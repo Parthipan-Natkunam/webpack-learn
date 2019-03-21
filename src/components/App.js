7ed5305c6eda38a328e7a8e566f3f6c0c7509bc3
parthipan90 <parthipan.natkunam@snapwiz.com>
2019-03-21 12:56:25 +0530

    Initial commit to git repo with webpack and babel configs

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+import '../styles/style.css';
+
+export class App extends Component {
+ render() {
+  return <p>Hi, I've been packed, and transpiled using webpack</p>
+ }
+}
